632754c7fe47e1496294a2eb9c95ee3188d10a55
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-28 18:24:55 +0000

    Corrigi o shift left para ser por 3 em vez de 2.

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -14,7 +14,7 @@ namespace SmoothOperator
             n++;
             Console.WriteLine(n);
             Console.WriteLine($"{n/2}");
-            Console.WriteLine($"{n<<2}");
+            Console.WriteLine($"{n<<3}");
             Console.WriteLine($"{n^5}");
             bool con = n > 10;
             Console.WriteLine(con);
