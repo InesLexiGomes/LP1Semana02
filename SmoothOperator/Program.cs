6e691cbf20797422ad9dee33522da0c61e584c1d
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-28 18:13:23 +0000

    Faz incremento e decremento do valor pedido ao utilizador.

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não-negativo: ");
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+            n--;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+
+        }
+    }
+}
