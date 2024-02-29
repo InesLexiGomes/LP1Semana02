67635e8927933b88d68c632831cf6a6d17fd9b9e
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-29 22:27:58 +0000

    ChatGPTOne agora não sai imediatamente do ciclo.

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -7,34 +7,42 @@ namespace ChatGPTOne
         private static void Main(string[] args)
         {
             // Inicialização da variável response.
-            string response;
+            string question, response;
 
-            // Questionar o utilizador
-            Console.Write("Place your question? ");
-            string question = Console.ReadLine();
+            question = "Start.";
 
-            // Verifica as perguntas e responde de acordo
-            switch (question)
+            while (question != "Quit")
             {
-                case "Are you gay?":
-                    response = "Yes.";
-                    break;
-                case "Are you french?":
-                    response = "Nein, ich bin Deutch";
-                    break;
-                case "Hello World?":
-                    response = "Programmed to work and not to feeeel-";
-                    break;
-                case "Are you addicted to video games?":
-                    response = "Obviously!";
-                    break;
-                default:
-                    response = "I am not programmed to answer your question.";
-                    break;
-            }
+                // Questionar o utilizador
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
 
+                // Verifica as perguntas e responde de acordo
+                switch (question)
+                {
+                    case "Are you gay?":
+                        response = "Yes.";
+                        break;
+                    case "Are you french?":
+                        response = "Nein, ich bin Deutch";
+                        break;
+                    case "Hello World?":
+                        response = "Programmed to work and not to feeeel-";
+                        break;
+                    case "Are you addicted to video games?":
+                        response = "Obviously!";
+                        break;
+
+                    case "Quit":
+                        continue;
+                        
+                    default:
+                        response = "I am not programmed to answer your question.";
+                        break;
+                }
             // Print da resposta
             Console.WriteLine(response);
+            }
         }
     }
 }
